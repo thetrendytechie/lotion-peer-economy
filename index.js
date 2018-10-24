14fbd5777b129943e3c991179933518929458ef9
Sage Franch <trendytechie@live.com>
2018-10-24 18:03:54 -0400

    chat framework working

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-// async function main(){
-//     let port = process.env.PORT || (await getPort())
-//     console.log('starting blockchain interface on port ' + port + '...\n')
-//     console.log(
-//     ` 
-//     chain state : http://localhost:${port}/state
-//     transactions: http://localhost:${port}/txs
+let initialState = {
+    offers: [
+        { poster: 'sage', service: 'Blockchain Crash Course', hours: 1 }
+    ],
+    //genesisPath: './genesis.json',
+    //peers: peers ? [peers] : [],
+    logTendermint: false //if true, this would show all output from underlying tendermint process
+}
 
-//     `
-//     )
-//     let opts = {
-//         port,
-//         initialState: {
-//             messages: [
-//                 { sender: 'keppel', message: 'to demo lotion' },
-//                 { sender: 'keppel', message: 'i made a haiku blockchain' },
-//                 { sender: 'keppel', message: 'you must write new ones' }
-//             ]
-//         },
-//         peers: peers ? [peers] : [],
-//         logTendermint: false
-
-//     }
-// }
-
-let app = require('lotion')({
-    initialState: { count: 0 }
-  });
+let app = require('lotion')(initialState);
   
   app.use((state, tx) => {
     state.count++
